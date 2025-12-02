7e8a3e9c33348674f6430d2bb7a5b0247217c0af
SaqO(n*logn) <ONLYPROGRAMMING@Sargiss-MacBook-Air.local>
2025-12-02 16:30:02 +0400

    ADD: routes add login resgister forgotpassword and resetpassword

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { register, login } from '../services/auth.service.js';
+import { register, login, forgotPassword, resetPassword } from '../services/auth.service.js';
 
 const router = express.Router();
 router.post('/register', register);
 router.post('/login', login);
+router.post('/forgot-password', forgotPassword);
+router.post('/reset-password', resetPassword);
 
 export default router;
 
--- services/auth.service.js
+++ services/auth.service.js
@@ -1,4 +1,5 @@
 import jwt from "jsonwebtoken";
+import crypto from "crypto";
 import User from "../models/user.model.js";
 import { hashPassword, comparePassword } from "../utils/password.utils.js";
 
@@ -34,6 +35,67 @@ export async function login(req, res) {
       { expiresIn: "7d" }
     );
     res.json({ message: "Login successful", token });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+}
+
+import crypto from "crypto";
+import User from "../models/user.model.js";
+import { sendEmail } from "../utils/email.js";
+
+export async function forgotPassword(req, res) {
+  try {
+    const { email } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user)
+      return res.status(404).json({ message: "User not found" });
+
+    const token = crypto.randomBytes(32).toString("hex");
+    user.resetPasswordToken = token;
+    user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
+    await user.save();
+
+    const resetLink = `http://localhost:5002/reset-password/${token}`;
+
+    await sendEmail(
+      user.email,
+      "Password Reset Request",
+      `
+        <h1>Reset Your Password</h1>
+        <p>Click the link below to reset your password:</p>
+        <a href="${resetLink}">${resetLink}</a>
+      `
+    );
+
+    res.json({ message: "Password reset email sent!" });
+
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+}
+
+
+export async function resetPassword(req, res) {
+  try {
+    const { token, newPassword } = req.body;
+
+    const user = await User.findOne({
+      resetPasswordToken: token,
+      resetPasswordExpires: { $gt: Date.now() }
+    });
+
+    if (!user)
+      return res.status(400).json({ message: "Invalid or expired token" });
+
+    user.password = await hashPassword(newPassword);
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpires = undefined;
+    await user.save();
+
+    res.json({ message: "Password has been reset successfully" });
+
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
