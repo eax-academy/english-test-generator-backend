b0559c3a4512c88ec37bba68ee316a5d237e0cb1
Sargis Abgaryan <sargisa017@gmail.com>
2026-01-29 20:39:20 +0400

    new button in Admin dashboard

--- routes/admin.submissions.routes.js
+++ routes/admin.submissions.routes.js
@@ -1,14 +1,16 @@
 import express from 'express';
-import { 
-    getAllSubmissions, 
-    getSubmissionById, 
-    deleteSubmission 
+import {
+    getAllSubmissions,
+    getSubmissionById,
+    deleteSubmission,
+    updateSubmission
 } from '../controllers/admin.submission.controller.js';
 
 const router = express.Router();
 
 router.get('/', getAllSubmissions);
 router.get('/:id', getSubmissionById);
+router.put('/:id', updateSubmission);
 router.delete('/:id', deleteSubmission);
 
 export default router;
