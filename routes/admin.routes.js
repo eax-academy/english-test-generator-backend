296b8088bc6a9c539f3e40f8822f03cb6c422116
Veronika-Te <veronterterian@gmail.com>
2026-03-01 20:44:42 +0400

    Add admin settings and seed script

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,17 +1,20 @@
 import express from "express";
+
 import { verifyToken, isAdmin } from "../middleware/auth.middleware.js";
 
-import adminLog from "../routes/admin.logs.js";
+import adminLog from "./admin.logs.js";
 import adminWords from "./admin.words.routes.js";
 import adminSubmissions from "./admin.submissions.routes.js";
 import adminQuizzes from "./admin.quizzes.routes.js";
-import adminUsers from "./admin.users.routes.js";
+import adminUsersSubRouter from "./admin.users.routes.js";
 
 import {
   getDashboardStats,
   getAllTests,
   getAllResults,
   saveResult,
+  getAllUsers,
+  deleteUser,
 } from "../controllers/admin.controller.js";
 
 const router = express.Router();
@@ -22,14 +25,16 @@ router.use("/logs", isAdmin, adminLog);
 router.use("/words", isAdmin, adminWords);
 router.use("/submissions", isAdmin, adminSubmissions);
 router.use("/quizzes", isAdmin, adminQuizzes);
-router.use("/users", isAdmin, adminUsers);
+router.use("/users-list", isAdmin, adminUsersSubRouter);
 
-// Dashboard
 router.get("/dashboard", isAdmin, getDashboardStats);
 
-// Results (Tests & Attempts)
+router.get("/users", isAdmin, getAllUsers);
+router.delete("/users/:id", isAdmin, deleteUser);
+
 router.get("/results", isAdmin, getAllResults);
-router.post("/results", saveResult)
 router.get("/tests", isAdmin, getAllTests);
+
+router.post("/results", saveResult);
 
 export default router;
